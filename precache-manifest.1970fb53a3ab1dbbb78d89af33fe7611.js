59c47ed0044c2ea6df2892d516d12e6974e04366
Andrew Dimmer <contactandrewd@gmail.com>
2020-06-25 19:54:02 -0400

    Updates

---
+++ precache-manifest.1970fb53a3ab1dbbb78d89af33fe7611.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b619cdb896a816aa83640c2eeb9be8a8",
+    "url": "/endangared-defender/index.html"
+  },
+  {
+    "revision": "3f71ec52797e79a4484d",
+    "url": "/endangared-defender/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "275c997a4ca23dd01b8d",
+    "url": "/endangared-defender/static/js/2.73032a36.chunk.js"
+  },
+  {
+    "revision": "a9877697f38e498213d7e52eef771b72",
+    "url": "/endangared-defender/static/js/2.73032a36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f71ec52797e79a4484d",
+    "url": "/endangared-defender/static/js/main.a1972e18.chunk.js"
+  },
+  {
+    "revision": "5f92928583c58667f8f2",
+    "url": "/endangared-defender/static/js/runtime-main.df44496d.js"
+  }
+]);
